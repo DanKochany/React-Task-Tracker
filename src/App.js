5002325551a9c830ab9e5b84a643d5974c84497e
Daniel Kochany <danielkochany@gmail.com>
2022-09-28 15:16:59 -0300

    utilizing some mock database to simulate a backend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AddTask from './components/AddTask';
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
@@ -6,38 +6,53 @@ import Tasks from "./components/Tasks";
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
 
-  const [tasks, setTasks] = useState([
-    {
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false,
-    },
-  ])
+  const [tasks, setTasks] = useState([])
 
+
+  //useEffect é utilizado quando você quer algo para carregar quando a tela carrega
+  useEffect(() =>{
+    const getTasks = async () =>{
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+  //fetch tasks
+  const fetchTasks = async ()=>{
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
 
   /*  Add Task */
-  const addTask = (task)=>{
-    const id = Math.floor(Math.random()*10000) + 1
+  const addTask = async (task)=>{
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+
+/*     const id = Math.floor(Math.random()*10000) + 1
     const newTask = {id, ...task}
-    setTasks([...tasks, newTask])
+    setTasks([...tasks, newTask]) */
   }
 
   /* Delete task */
 
-  const deleteTask = (id) =>{
+  const deleteTask = async (id) =>{
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+
     setTasks(tasks.filter((task)=>task.id !== id))
   }
 
@@ -49,8 +64,8 @@ function App() {
   return (
     <div className="container">
       <Header 
-        onAdd={()=> setShowAddTask(!showAddTask)}
-        showAdd={showAddTask}
+        onAdd={()=> setShowAddTask(!showAddTask)} 
+        showAdd={showAddTask} 
       />
       {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (
